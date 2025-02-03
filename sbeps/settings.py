3903853a0ab920fc37d3a12d5d6684d7c28124ee
leterDieu <leterdieu@proton.me>
2025-02-03 18:14:33 +0300

    models rework

--- sbeps/settings.py
+++ sbeps/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS","127.0.0.1").split(",")
 # Application definition
 
 INSTALLED_APPS = [
+    'training.apps.TrainingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- training/models.py
+++ training/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Test(models.Model):
+class Exum(models.Model):
     name = models.CharField(max_length=64)
     description = models.CharField(max_length=256, default='')
 
 class Question(models.Model):
-    test = models.ForeignKey(Test, on_delete=models.CASCADE)
+    exum = models.ForeignKey(Exum, on_delete=models.CASCADE)
     text = models.CharField(max_length=128)
 
 class Answer(models.Model):
@@ -14,11 +14,9 @@ class Answer(models.Model):
     text = models.CharField(max_length=128)
     is_correct = models.BooleanField()
 
-class UserResult(models.Model):
-    test = models.ForeignKey(Test, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)    # test_id
-
 class UserQuestionResult(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    exum = models.ForeignKey(Exum, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
-    user_result = models.ForeignKey(UserResult, on_delete=models.CASCADE)
+    answer_datetime = models.DateTimeField(auto_now_add=True)
