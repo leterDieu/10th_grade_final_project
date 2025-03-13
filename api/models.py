2b447a71f2df2fae73479eb7365eb747346b704f
leterDieu <leterdieu@proton.me>
2025-03-13 10:43:34 +0300

    .

--- api/models.py
+++ api/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 class Exam(models.Model):
     name = models.CharField(max_length=64)
     description = models.CharField(max_length=256, default='')
+    pud_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
--- training/views.py
+++ training/views.py
@@ -16,13 +16,11 @@ from django.db.models import IntegerField
 import random
 from training.forms import QuestionForm, BaseQuestionFormSet
 from django.forms import formset_factory
+import pandas as pd
+from typing import Any
 
 
-def index(request, top_n: int = 5):
-    uqr = UserQuestionResult.objects.all().\
-    select_related('answer').select_related('exam')
-    uqr = uqr.annotate(is_correct_int = Cast('answer__is_correct', IntegerField()))
-
+def get_exam_stats(uqr: UserQuestionResult) -> dict[str, list[dict[str, Any]]]:
     exam_stats = {}
     for el in uqr:
         if el.exam_id not in exam_stats:
@@ -57,11 +55,34 @@ def index(request, top_n: int = 5):
             'answers': el['answers']}
         for i, (id, el) in enumerate(exam_stats_by_popularity.items())]
 
+    return {
+        'accuracy': exam_stats_by_accuracy_readable,
+        'popularity': exam_stats_by_popularity_readable,
+    }
+
+def index(request, top_n: int = 5, latest_n: int = 10):
+    uqr = UserQuestionResult.objects.all().\
+    select_related('answer').select_related('exam')
+    uqr = uqr.annotate(is_correct_int = Cast('answer__is_correct', IntegerField()))
+
+    uqr_users = UserQuestionResult.objects.filter(user=request.user).\
+    select_related('answer').select_related('exam')
+    uqr_users = uqr_users.annotate(is_correct_int = Cast('answer__is_correct', IntegerField()))
+
+    exam_stats_overall = get_exam_stats(uqr)
+    exam_stats_users = get_exam_stats(uqr_users)
+
+    latest_exams = Exam.objects.all().order_by('pud_date')[:latest_n]
+
     template = loader.get_template("training/index.html")
     context = {
-        'exam_stats_by_accuracy_readable': exam_stats_by_accuracy_readable[:top_n],
-        'exam_stats_by_popularity_readable': exam_stats_by_popularity_readable[:top_n],
+        'exam_stats_overall_accuracy': exam_stats_overall['accuracy'][:top_n],
+        'exam_stats_overall_popularity': exam_stats_overall['popularity'][:top_n],
+        'exam_stats_users_accuracy': exam_stats_users['accuracy'][:top_n],
+        'exam_stats_users_popularity': exam_stats_users['popularity'][:top_n],
         'top_n': top_n,
+        'latest_exams': latest_exams[::-1],
+        'latest_n': latest_n
     }
 
     return HttpResponse(template.render(context, request))
@@ -142,15 +163,41 @@ def exam_result(request, result_session_id):
         pass
     else:
         uqr_objects = UserQuestionResult.objects.filter(result_session=result_session_id)
-        answers_info = []
+        answers_info = {
+            'question': [],
+            'user_answer': [],
+            'right_answer': [],
+            'is_correct': []
+        }
+        answers_total = {
+            'answers': [0],
+            'right_answers': [0],
+            'accuracy': []
+        }
         for uqr in uqr_objects:
             right_answer_text = Answer.objects.get(question=uqr.question.id, is_correct=True).text
-            answers_info.append(
-                f'''
-                question: {uqr.question.text},
-                user answer: {uqr.answer.text},
-                right answer: {right_answer_text},
-                is correct: {uqr.answer.is_correct}
-                '''
-            )
-        return HttpResponse(answers_info)
+            answers_info['question'].append(uqr.question.text)
+            answers_info['user_answer'].append(uqr.answer.text)
+            answers_info['right_answer'].append(right_answer_text)
+            answers_info['is_correct'].append(uqr.answer.is_correct)
+
+            answers_total['answers'][0] += 1
+            if uqr.answer.is_correct:
+                answers_total['right_answers'][0] += 1
+
+
+        answers_total['accuracy'].append(
+            f'{round(100 * answers_total['right_answers'][0] / answers_total['answers'][0], 2)}%'
+        )
+
+        df = pd.DataFrame.from_dict(answers_info)
+        df_total = pd.DataFrame.from_dict(answers_total)
+
+        template = loader.get_template("training/exam_result.html")
+        context = {
+            'df': df,
+            'df_total': df_total,
+            'exam_id': uqr_objects[0].exam.id
+        }
+
+        return HttpResponse(template.render(context, request))
