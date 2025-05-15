399da4753e8f98bf1b3871298a68d4dfc8837634
leterDieu <leterdieu@proton.me>
2025-05-15 09:09:20 +0300

    hotfix: host

--- sbeps/settings.py
+++ sbeps/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", '*')
 DEBUG = bool(os.environ.get("DEBUG", default=0))
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "127.0.0.1").split(",")
+ALLOWED_HOSTS.append('95.165.90.137')
 
 
 # Application definition
